265dedb04a88821348d71518170b953a6dc7fff9
deepaksahu562 <deepak.sahu562@gmail.com>
2024-02-24 01:15:59 +0530

    7nd-commit

--- src/main/java/edu/dks/cicd/demo/CicdDemoApplication.java
+++ src/main/java/edu/dks/cicd/demo/CicdDemoApplication.java
@@ -2,10 +2,17 @@ package edu.dks.cicd.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CicdDemoApplication {
 
+	@GetMapping("/")
+	public String service(){
+		return "This github action application....!!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CicdDemoApplication.class, args);
 	}
